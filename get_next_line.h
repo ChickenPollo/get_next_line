e4d49e83bac2bac64a834a23244f5b9c21ddc9f8
Luis david Martin de alba <luimarti@student.42.us.org>
2020-03-02 13:09:48 -0800

    Revert "changes, more changes"
    This reverts commit f48fff7adc5c774055f10c41bbca917382e5fec5.

--- get_next_line.h
+++ get_next_line.h
@@ -14,20 +14,15 @@
 # define GET_NEXT_LINE_H
 
 # include "libft/libft.h"
-# include <fcntl.h>
-# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 # define BUFF_SIZE 32
-# define IS_ALLO(x) if (!x) return (-1);
-# define CONT(x) ((t_file*)((x->content)))->content
-# define START(x) ((t_file*)((x->content)))->start_content
-
 
 typedef	struct	s_file
 {
 	int				fd;
-	char			*content;
-	char			*start_content;
+	char			*buffer;
 	struct s_file	*next;
 }				t_file;
 
